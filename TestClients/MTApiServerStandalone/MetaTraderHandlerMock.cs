ba40a6efed03444cb7aaabad482c21d3f7d175c3
Jan Scheperski <jan.scheperski@gmail.com>
2022-11-27 01:18:58 +0100

    added standalone server app for testing

---
+++ TestClients/MTApiServerStandalone/MetaTraderHandlerMock.cs
@@ -0,0 +1,17 @@
+﻿using MTApiService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTApiServerStandalone
+{
+    internal class MetaTraderHandlerMock : IMetaTraderHandler
+    {
+        public void SendTickToMetaTrader(int handle)
+        {
+
+        }
+    }
+}
---
+++ TestClients/MTApiServerStandalone/Program.cs
@@ -0,0 +1,20 @@
+﻿using MTApiService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTApiServerStandalone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var handlerMock = new MetaTraderHandlerMock();
+            Mt5Expert expert = new Mt5Expert(99, "[ANY100]", 10000, 9999, handlerMock, true);
+            MtAdapter.GetInstance().AddExpert(8228, expert);
+            Console.ReadLine();
+        }
+    }
+}
